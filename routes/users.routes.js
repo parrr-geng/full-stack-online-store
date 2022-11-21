687ee4cba331a3bed59e6f3d827c6554432f3907
ssafali <safaksafali@gmail.com>
2022-11-21 10:37:05 +0100

    Post login info done.

---
+++ routes/users.routes.js
@@ -0,0 +1,70 @@
+const router = require("express").Router();
+const bcryptjs = require("bcryptjs");
+const User = require("../models/User.model");
+
+/* GET signup page */
+router.get("/signup", (req, res, next)=>{
+    res.render("users/signup.hbs");
+});
+
+/* POST signup info */
+router.post("/signup", (req, res, next)=>{
+    const {firstName, lastName, email, password} = req.body;
+    if (!firstName || !lastName || !email || !password){
+        res.render("users/signup.hbs", {message: "PLEASE FILL IN ALL THE REQUIRED FIELDS."})
+    }
+
+    if (password.length < 6){
+        res.render("users/signup.hbs", {message: "Password should have at least 6 characters."})
+    }
+
+    User.findOne(email)
+    .then((userFromDB)=>{
+        if (userFromDB){
+            res.render("users/signup.hbs", {message: "This email has already been registered."});
+            return;
+        } else {
+            const salt = bcryptjs.genSaltSync();
+            const hashedPassword = bcryptjs.hashSync(password, salt);
+
+            User.create({firstName, lastName, email, password})
+            .then(userFromDB => {
+                res.redirect("/login");
+            })
+        }
+        
+    })
+    .catch(err => next(err));
+})
+
+/* GET login page */
+router.get("/login", (req, res, next) => {
+    res.render("users/login.hbs")
+  })
+/* Post login info */
+router.post("/login", (req, res, next) => {
+  const { email, password } = req.body
+
+  // Find user
+  User.findOne({ email })
+    .then(userFromDB => {
+      if (userFromDB === null) {
+        // User not found
+        res.render("login", { message: "User not found" })
+        return;
+      }
+      // User found in database
+      // Check if password from input form matches hashed password from database
+      if (bcryptjs.compareSync(password, userFromDB.password)) {
+        // Password is correct => Login user
+        req.session.user = userFromDB
+        res.redirect("/profile")
+      } else {
+        res.render("login.hbs", { message: "Wrong credentials" })
+        return;
+      }
+    })
+})
+
+
+module.exports = router;
