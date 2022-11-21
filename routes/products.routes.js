73201bd7e91fadc04738ae852d2d6558f2ebe617
Parrr Geng <parrr.geng@gmail.com>
2022-11-21 12:26:31 +0100

    database connected to mongo atlas

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -2,8 +2,25 @@ const router = require("express").Router();
 const Product = require("../models/Product.model");
 const {uploader, cloudinary} = require("../config/cloudinary");
 
-router.get("/product/add", (req, res, next)=>{
+//add a new product by the logged-in user
+router.get("/profile/:id/add-product", (req, res, next)=>{
     res.render("products/new-product.hbs");
+});
+
+//post the added product info from the logged-in user
+router.post("profile/:id/add-product", uploader.single("productImage"), (req, res, next)=>{
+    const {name, description, price} = req.body;
+    const imgName = req.file.originalname;
+    const imgPath = req.file.path;
+    const publicId = req.file.filename;
+    const seller = req.params.id;
+
+    Product.create({name, description, price, imgName, imgPath, publicId, seller})
+    .then(product => {
+        console.log(product);
+        res.redirect("/profile") //<-- how can I specify id in this?
+    })
+    .catch(err => next(err));
 })
 
 
