5da1522465b37759dcb60aeeb1508e1272da4bb1
Parrr Geng <parrr.geng@gmail.com>
2022-11-22 17:51:41 +0100

    initial styling

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,11 +2,16 @@ const router = require("express").Router();
 const Product = require('../models/Product.model.js');
 
 /* GET home page */
-router.get("/", (req, res, next) => {
+router.get("/", (req, res, next)=>{
+  res.render("index");
+});
+
+router.get("/all", (req, res, next) => {
   Product.find()
     .then((productInfo) => {
-        res.render('index', { products: productInfo});
+        res.render('products/all-products.hbs', { products: productInfo});
     }).catch(err => console.log(err))
     
 });
+
 module.exports = router;
